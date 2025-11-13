5833014ee8024022ce3df412f79d7204af9541f0
Dylan Rodríguez <dylanrodriguezchavarria@gmail.com>
2025-11-12 19:56:13 -0600

    Para cambiar los archivos

--- proyecto/src/ReemplazoEquipos/OperacionG.d.ts
+++
@@ -1,9 +0,0 @@
-interface OperacionG {
-    anioInicial: number;
-    anioFinal: number;
-    costoAnioInicial: number;
-    costoAnioFinal: number;
-    resultado: number;
-}
-
-export type { OperacionG };
--- proyecto/src/ReemplazoEquipos/SolucionReemplazoEquipos.d.ts
+++
@@ -1,8 +0,0 @@
-import { SolucionG } from "./SolucionG";
-
-interface SolucionReemplazoEquipos {
-    soluciones: Map<string, SolucionG>;
-    planesReemplazo: number[][];
-}
-
-export type { SolucionReemplazoEquipos };
